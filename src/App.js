ae0d08a3e841e7cbed5a983e1bdeac6618e6344a
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-09-05 08:47:28 -0300

    Displaying the joke retrived from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
-import ButtonTest from "./components/ButtonTest";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+import useJokes from "./hooks/useJokes";
 
 function App() {
+  const [loading, error, jokes, setStartFetch] = useJokes();
+
   return (
     <div>
-      <ButtonTest />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => setStartFetch(true)}
+      >
+        Call API
+      </Button>
+      <Typography>{JSON.stringify(jokes)}</Typography>
     </div>
   );
 }
