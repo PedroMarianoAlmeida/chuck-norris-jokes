54467ecb389202f813cc8d389cd3f198bd87a88b
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-09-05 07:52:33 -0300

    Removing Unecessary Code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
-import ButtonTest from "./components/ButtonTest";
-import IconTest from "./components/IconTest";
-import TypographyTest from "./components/TypographyTest";
-import LabTest from "./components/LabTest";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <ButtonTest />
-        <IconTest />
-        <TypographyTest />
-        <LabTest />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
