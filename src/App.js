299aba12fc8081efb988774c8dbfae330d4f2020
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-09-05 07:49:13 -0300

    Material UI Lab Instaled

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import ButtonTest from "./components/ButtonTest";
 import IconTest from "./components/IconTest";
 import TypographyTest from "./components/TypographyTest";
+import LabTest from "./components/LabTest";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <ButtonTest />
         <IconTest />
         <TypographyTest />
+        <LabTest />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/LabTest.jsx
@@ -0,0 +1,7 @@
+import Skeleton from "@material-ui/lab/Skeleton";
+
+const LabTest = () => {
+  return <Skeleton variant="rect" width={210} height={118} />;
+};
+
+export default LabTest;
