64a041d85cd70c477749bd3430c16e6213b60faa
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-09-05 07:33:09 -0300

    Material UI Core instaled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ButtonTest from "./components/ButtonTest";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonTest />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/ButtonTest.jsx
@@ -0,0 +1,11 @@
+import Button from "@material-ui/core/Button";
+
+const ButtonTest = () => {
+  return (
+    <Button variant="contained" color="primary">
+      Primary
+    </Button>
+  );
+};
+
+export default ButtonTest;
