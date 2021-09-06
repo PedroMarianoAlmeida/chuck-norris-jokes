0bae9d08df28cc813fd0c8cf3d398aed20239d38
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-09-06 20:46:45 -0300

    Id Selected List implemented

--- src/components/JokeForm/IdTab.jsx
+++ src/components/JokeForm/IdTab.jsx
@@ -5,8 +5,28 @@ import IconButton from "@material-ui/core/IconButton";
 import AddIcon from "@material-ui/icons/Add";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import Chip from "@material-ui/core/Chip";
 
 import { JokeFormContext } from "./../../contexts/JokeFormContext";
+
+const IdSelectedList = ({ idList }) => {
+  return (
+    <Box display="flex" mb={2} flexWrap="wrap" alignItems="center">
+      {!!idList.length && (
+        <>
+          <Typography>IDs: </Typography>
+          {idList.map((id) => (
+            <>
+              <Box m={1}>
+                <Chip label={id} />
+              </Box>
+            </>
+          ))}
+        </>
+      )}
+    </Box>
+  );
+};
 
 const IdTab = () => {
   const { jokeIds, setJokeIds, setCurrentTab } = useContext(JokeFormContext);
@@ -42,24 +62,29 @@ const IdTab = () => {
 
   return (
     <>
-      <TextField
-        id="outlined-basic"
-        label="id number"
-        variant="outlined"
-        value={currentId}
-        onChange={handleChange}
-      />
-      <Box ml={2} display="inline">
-        <IconButton
-          aria-label="add"
-          color="primary"
-          disabled={!!errorMessage}
-          onClick={handleClick}
-        >
-          <AddIcon />
-        </IconButton>
+      <Box display="flex" flexDirection="column">
+        <IdSelectedList idList={jokeIds} />
+        <Box display="flex" justifyContent="center">
+          <TextField
+            id="outlined-basic"
+            label="id number"
+            variant="outlined"
+            value={currentId}
+            onChange={handleChange}
+          />
+          <Box ml={2} display="inline">
+            <IconButton
+              aria-label="add"
+              color="primary"
+              disabled={!!errorMessage || errorMessage !== ""}
+              onClick={handleClick}
+            >
+              <AddIcon />
+            </IconButton>
+          </Box>
+          <Typography color="error">{errorMessage}</Typography>
+        </Box>
       </Box>
-      <Typography color="error">{errorMessage}</Typography>
     </>
   );
 };
