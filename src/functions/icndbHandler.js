f953b62f50a7a7b74685ac0100e53240c24b78de
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-09-07 12:21:56 -0300

    Jokes by category routes implemented

--- src/functions/icndbHandler.js
+++ src/functions/icndbHandler.js
@@ -1,3 +1,57 @@
+const categoryRoute = (categories) => {
+  const routeByCategoryOptions = [
+    { none: true, explicity: true, nerdy: true, route: "/random" },
+    {
+      none: true,
+      explicity: true,
+      nerdy: false,
+      route: "/random?exclude=[nerdy]",
+    },
+    {
+      none: true,
+      explicity: false,
+      nerdy: true,
+      route: "/random?exclude=[explicit]",
+    },
+    {
+      none: true,
+      explicity: false,
+      nerdy: false,
+      route: "/random?exclude=[nerdy,explicit]",
+    },
+    {
+      none: false,
+      explicity: true,
+      nerdy: true,
+      route: "/random?limitTo=[nerdy,explicit] ",
+    },
+    {
+      none: false,
+      explicity: true,
+      nerdy: false,
+      route: "/random?limitTo=[explicit] ",
+    },
+    {
+      none: false,
+      explicity: false,
+      nerdy: true,
+      route: "/random?limitTo=[nerdy]",
+    },
+  ];
+
+  const matchCategories = (options) => {
+    return (
+      options.none === categories.none &&
+      options.explicity === categories.explicity &&
+      options.nerdy === categories.nerdy
+    );
+  };
+
+  const routeFiltered = routeByCategoryOptions.filter(matchCategories);
+  console.log(routeFiltered[0].route);
+  return routeFiltered[0].route;
+};
+
 const icndbHandler = (parametersData) => {
   const baseURL = "http://api.icndb.com/jokes";
   let complementURL = "";
@@ -15,6 +69,11 @@ const icndbHandler = (parametersData) => {
     case "random":
       complementURL += "/random";
       if (numberOfRandomJokes > 1) complementURL += `/${numberOfRandomJokes}`;
+      break;
+
+    case "categories":
+      console.log(categories);
+      complementURL += categoryRoute(categories);
       break;
   }
 
